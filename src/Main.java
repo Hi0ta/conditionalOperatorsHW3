c1f6330af0a170417264382a4ab1010eac64738a
Ann <niyta@mail.ru>
2022-08-05 23:15:38 +0500

    ДЗ сделано

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+
+         int clientOS = 0;
+        if (clientOS == 0) {
+        System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1) {
+        System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        // Задание 2
+        int ClientOS = 1;
+        int clientDeviceYear = 2015;
+        if (ClientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");}
+            else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        }
+        if (ClientOS == 1) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android  по ссылке");}
+            else {
+                System.out.println("Установите версию приложения для Android  по ссылке");
+            }
+
+
+        }
     }
+
 }
